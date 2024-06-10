23ac66900667237eb9cd849c6b2d0bb295583b4b
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-06-11 06:40:00 +0800

    1

--- src/zp118_批量上传图片.js
+++ src/zp118_批量上传图片.js
@@ -2,57 +2,39 @@ import React from "react"
 import css from "./zp118_批量上传图片.css"
 
 function render(ref) {
-    const { exc, render, props, arr = [] } = ref
-    const isUploading = arr.find(a => a.startsWith("blob"))
+    let { props } = ref
+    let arr = gets(ref)
     return <React.Fragment>
-        {arr.map((a, i) => <div className={"zp118B zp118_" + i + (a.startsWith("blob") ? " zp118U" : " zp118Z")} onClick={() => preview(ref, a)} key={a + i}>
-            <div className="zp118progress"/>
-            <img src={a.startsWith("blob") || a.endsWith("svg") ? a : a + "?x-oss-process=image/resize,m_fill,h_300,w_300"}/>
-            {!isUploading && <i onClick={e => remove(ref, i, e)} className="zdel zp118del"/>}
-            {!isUploading && EL.handle}
+        {arr.map((a, i) => <div className="zp118B zp118Z" onClick={() => preview(ref, a)} key={a + i}>
+            <img src={a.endsWith("svg") ? a : a + "?x-oss-process=image/resize,m_fill,h_300,w_300"}/>
+            <i onClick={e => {e.stopPropagation(); ref.exc('confirm("确定要删除吗？")', {}, () => {arr.splice(i, 1); ref.form ? ref.form[props.dbf] = arr : ref.setForm(props.dbf, arr); ref.exc('render()')})}} className="zdel zp118del"/>
+            <i className="zmove zp118handler"/>
         </div>)}
-        {EL.网盘}
+        {ref.ing.map((a, i) => <div className={"zp118B zp118_" + i + " zp118U"} onClick={() => preview(ref, a)} key={a + i}>
+            <div className="zp118progress"/>
+            <img src={a}/>
+        </div>)}
         <div className="zp118B">
-            <div>{EL.camera}<label>{props.dbf ? props.label || "上传图片" : "请配置表单字段"}</label></div>
+            <div className={props.noLabel ? "zp118noLabel" : ""}><span className="zphoto"><span/></span><label>{props.noLabel ? "" : (props.label || "上传图片")}</label></div>
             <input onChange={e => onChange(ref, e)} type="file" accept="image/*" multiple="multiple"/>
-            {!!ref.props.url && <span onClick={() => popUrl(ref)}>URL</span>}
+            {!!props.url && <span className="zp118url" onClick={() => popUrl(ref)}>URL</span>}
             {ref.modal}
         </div>
     </React.Fragment>
 }
 
 function init(ref) {
-    const { getForm, id, exc, props, render } = ref
-    if (!getForm) return exc('warn("请置于表单容器中")')
-    const arr = getForm(props.dbf)
-    if (Array.isArray(arr)) {
-        ref.arr = [...arr]
-    } else {
-        if (arr) exc('warn("表单字段必须是数组")')
-        ref.arr = []
-    }
-    if (props.gallery) {
-        EL.网盘 = render({ t: "Plugin", p: { ID: "zp101", P: { mineOnly: true, onSelect: '$("#' + id + '").add(url)', type: "i", label: "图库" } } }, id + "_0")
-        ref.container.add = url => {
-            ref.arr.push(url)
-            let arr = getForm(props.dbf)
-            if (!Array.isArray(arr)) arr = []
-            arr.push(url)
-            ref.setForm(props.dbf, arr)
-        }
-    }
+    const { id, exc, props, render } = ref
+    ref.ing = []
     exc('load("https://z.zccdn.cn/vendor/Sortable_1.13.0.js")', {}, () => {
         new Sortable(ref.container, {
             animation: 150,
             forceFallback: true,
             fallbackTolerance: 5,
             onSort: e => {
-                let arr = getForm(props.dbf)
-                if (!Array.isArray(arr)) arr = []
+                let arr = gets(ref)
                 arr.splice(e.newDraggableIndex, 0, arr.splice(e.oldDraggableIndex, 1)[0])
-                ref.setForm(props.dbf, arr)
-                ref.arr = [...arr]
-                render()
+                ref.form ? ref.form[props.dbf] = arr : ref.setForm(props.dbf, arr)
             },
             handle: "#" + id + " .zp118handler",
             draggable: ".zp118Z",
@@ -62,50 +44,49 @@ function init(ref) {
     })
 }
 
+function gets(ref) {
+    let { dbf, form } = ref.props
+    let arr
+    if (form) {
+        ref.form = typeof form == "string" ? ref.excA(form) : form
+        if (typeof ref.form == "object") arr = ref.form[dbf]
+    } else if (ref.getForm) {
+        arr = ref.getForm(dbf)
+    }
+    return Array.isArray(arr) ? arr : []
+}
+
 function onChange(ref, e) {
     const { exc, render, props } = ref
     const arr = Array.from(e.target.files)
     if (!arr.length) return exc('warn("请选择图片")')
+    if (arr.find(a => a.size / 1048576 > (props.max || 5))) return exc(`warn("文件太大, 请压缩至${props.max || 5}M以下")`)
     arr.forEach((file, i) => setTimeout(() => {
         const x = URL.createObjectURL(file)
-        ref.arr.push(x)
+        ref.ing.push(x)
         render()
         exc('upload(file, option)', {
             file,
             option: {
                 onProgress: r => {
-                    $("#" + ref.id + " .zp118_" + ref.arr.indexOf(x) + " .zp118progress").innerHTML = r.percent + "%"
+                    $("#" + ref.id + " .zp118_" + ref.ing.indexOf(x) + " .zp118progress").innerHTML = r.percent + "%"
                 },
                 onSuccess: r => {
-                    let arr = ref.getForm(props.dbf)
-                    if (!Array.isArray(arr)) arr = []
-                    arr.push(r.url)
-                    ref.setForm(props.dbf, arr)
+                    let { props } = ref
                     preload(r.url, ref.container, () => {
-                        ref.arr.splice(ref.arr.indexOf(x), 1, r.url)
+                        ref.ing.splice(ref.ing.indexOf(x), 1)
                         URL.revokeObjectURL(x)
+                        ref.form ? ref.form[props.dbf].push(r.url) : ref.setForm(props.dbf, gets(ref).concat([r.url]))
+                        if (props.onSuccess) exc(props.onSuccess, { ...ref.ctx, $ext_ctx: ref.ctx, $val: gets(ref).concat([r.url]), ...r }, () => ref.exc("render()"))
                         exc('render()')
                     })
                 },
                 onError: r => {
                     exc(`alert("上传出错了", r.error)`, { r })
-                    ref.arr.splice(ref.arr.indexOf(x), 1)
-                    URL.revokeObjectURL(x)
                 }
             }
         })
     }, 2000 * i))
-}
-
-function remove(ref, i, e) {
-    e.stopPropagation()
-    ref.exc('confirm("确定要删除吗？")', {}, () => {
-        let arr = ref.getForm(ref.props.dbf)
-        arr.splice(i, 1)
-        ref.arr = [...arr]
-        ref.setForm(ref.props.dbf, arr)
-        ref.exc('render()')
-    })
 }
 
 function preload(url, container, onload) {
@@ -163,8 +144,8 @@ function upload(ref) {
         if (!r || r.ng.length) exc(`alert("上传出错了", reason)`, { reason: r ? JSON.stringify(r.ng, null, "\t") : "" })
         if (r.arr.length) {
             let arr = r.arr.map(a => a.url)
-            ref.arr = [...ref.arr, ...arr]
-            ref.setForm(ref.props.dbf, ref.arr)
+            arr = [...gets(ref), ...arr]
+            ref.form ? ref.form[ref.props.dbf] = arr : ref.setForm(ref.props.dbf, arr)
             exc('render()')
         }
     })
@@ -174,30 +155,39 @@ $plugin({
     id: "zp118",
     props: [{
         prop: "dbf",
-        type: "text",
-        label: "表单字段"
+        label: "字段名",
+        ph: "必填"
+    }, {
+        prop: "form",
+        label: "字段容器",
+        ph: "如不填则使用祖先节点的表单容器"
+    }, {
+        prop: "max",
+        type: "number",
+        label: "最大文件大小(单位:MB)",
+        ph: "默认最大5MB"
+    }, {
+        prop: "noLabel",
+        type: "switch",
+        label: "不显示文本"
     }, {
         prop: "label",
-        type: "text",
-        label: "[上传图片]文本"
-    }, {
-        prop: "gallery",
-        type: "switch",
-        label: "包含图库"
+        label: "[上传文件] 文本",
+        show: "!P.noLabel"
     }, {
         prop: "url",
         type: "switch",
         label: "允许通过URL上传"
+    }, {
+        prop: "onSuccess",
+        type: "exp",
+        label: "上传成功表达式",
+        ph: "$val"
     }],
     render,
     init,
     css
 })
-
-const EL = {
-    camera: <svg className="zsvg zp118camera" viewBox="0 0 1024 1024"><path d="M384 128l-78.933333 85.333333L170.666667 213.333333c-46.933333 0-85.333333 38.4-85.333333 85.333333l0 512c0 46.933333 38.4 85.333333 85.333333 85.333333l682.666667 0c46.933333 0 85.333333-38.4 85.333333-85.333333L938.666667 298.666667c0-46.933333-38.4-85.333333-85.333333-85.333333l-134.4 0L640 128 384 128zM512 768c-117.333333 0-213.333333-96-213.333333-213.333333s96-213.333333 213.333333-213.333333 213.333333 96 213.333333 213.333333S629.333333 768 512 768zM512 554.666667m-136.533333 0a6.4 6.4 0 1 0 273.066667 0 6.4 6.4 0 1 0-273.066667 0Z"/></svg>,
-    handle: <svg className="zsvg zp118handler" viewBox="0 0 1024 1024"><path d="M256 160a96 96 0 1 0 0 192 96 96 0 0 0 0-192z m0 512a96 96 0 1 0 0 192 96 96 0 0 0 0-192zM672 256a96 96 0 1 1 192 0 96 96 0 0 1-192 0z m96 416a96 96 0 1 0 0 192 96 96 0 0 0 0-192z"/></svg>,
-}
 
 
 /*
